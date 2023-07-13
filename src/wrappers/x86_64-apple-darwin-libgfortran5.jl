43b30fa2218c4cd570e21558d0e4b4234467ee95
jlbuild <juliabuildbot@gmail.com>
2023-07-13 02:32:06 +0000

    Sundials_jll build 6.5.1+0

--- src/wrappers/x86_64-apple-darwin-libgfortran5.jl
+++ src/wrappers/x86_64-apple-darwin-libgfortran5.jl
@@ -5,166 +5,166 @@ using CompilerSupportLibraries_jll
 using libblastrampoline_jll
 using SuiteSparse_jll
 JLLWrappers.@generate_wrapper_header("Sundials")
-JLLWrappers.@declare_library_product(libsundials_arkode, "@rpath/libsundials_arkode.4.dylib")
-JLLWrappers.@declare_library_product(libsundials_cvode, "@rpath/libsundials_cvode.5.dylib")
-JLLWrappers.@declare_library_product(libsundials_cvodes, "@rpath/libsundials_cvodes.5.dylib")
-JLLWrappers.@declare_library_product(libsundials_ida, "@rpath/libsundials_ida.5.dylib")
-JLLWrappers.@declare_library_product(libsundials_idas, "@rpath/libsundials_idas.4.dylib")
-JLLWrappers.@declare_library_product(libsundials_kinsol, "@rpath/libsundials_kinsol.5.dylib")
-JLLWrappers.@declare_library_product(libsundials_nvecmanyvector, "@rpath/libsundials_nvecmanyvector.5.dylib")
-JLLWrappers.@declare_library_product(libsundials_nvecserial, "@rpath/libsundials_nvecserial.5.dylib")
-JLLWrappers.@declare_library_product(libsundials_sunlinsolband, "@rpath/libsundials_sunlinsolband.3.dylib")
-JLLWrappers.@declare_library_product(libsundials_sunlinsoldense, "@rpath/libsundials_sunlinsoldense.3.dylib")
-JLLWrappers.@declare_library_product(libsundials_sunlinsolklu, "@rpath/libsundials_sunlinsolklu.3.dylib")
-JLLWrappers.@declare_library_product(libsundials_sunlinsollapackband, "@rpath/libsundials_sunlinsollapackband.3.dylib")
-JLLWrappers.@declare_library_product(libsundials_sunlinsollapackdense, "@rpath/libsundials_sunlinsollapackdense.3.dylib")
-JLLWrappers.@declare_library_product(libsundials_sunlinsolpcg, "@rpath/libsundials_sunlinsolpcg.3.dylib")
-JLLWrappers.@declare_library_product(libsundials_sunlinsolspbcgs, "@rpath/libsundials_sunlinsolspbcgs.3.dylib")
-JLLWrappers.@declare_library_product(libsundials_sunlinsolspfgmr, "@rpath/libsundials_sunlinsolspfgmr.3.dylib")
-JLLWrappers.@declare_library_product(libsundials_sunlinsolspgmr, "@rpath/libsundials_sunlinsolspgmr.3.dylib")
-JLLWrappers.@declare_library_product(libsundials_sunlinsolsptfqmr, "@rpath/libsundials_sunlinsolsptfqmr.3.dylib")
-JLLWrappers.@declare_library_product(libsundials_sunmatrixband, "@rpath/libsundials_sunmatrixband.3.dylib")
-JLLWrappers.@declare_library_product(libsundials_sunmatrixdense, "@rpath/libsundials_sunmatrixdense.3.dylib")
-JLLWrappers.@declare_library_product(libsundials_sunmatrixsparse, "@rpath/libsundials_sunmatrixsparse.3.dylib")
-JLLWrappers.@declare_library_product(libsundials_sunnonlinsolfixedpoint, "@rpath/libsundials_sunnonlinsolfixedpoint.2.dylib")
-JLLWrappers.@declare_library_product(libsundials_sunnonlinsolnewton, "@rpath/libsundials_sunnonlinsolnewton.2.dylib")
+JLLWrappers.@declare_library_product(libsundials_arkode, "@rpath/libsundials_arkode.5.dylib")
+JLLWrappers.@declare_library_product(libsundials_cvode, "@rpath/libsundials_cvode.6.dylib")
+JLLWrappers.@declare_library_product(libsundials_cvodes, "@rpath/libsundials_cvodes.6.dylib")
+JLLWrappers.@declare_library_product(libsundials_ida, "@rpath/libsundials_ida.6.dylib")
+JLLWrappers.@declare_library_product(libsundials_idas, "@rpath/libsundials_idas.5.dylib")
+JLLWrappers.@declare_library_product(libsundials_kinsol, "@rpath/libsundials_kinsol.6.dylib")
+JLLWrappers.@declare_library_product(libsundials_nvecmanyvector, "@rpath/libsundials_nvecmanyvector.6.dylib")
+JLLWrappers.@declare_library_product(libsundials_nvecserial, "@rpath/libsundials_nvecserial.6.dylib")
+JLLWrappers.@declare_library_product(libsundials_sunlinsolband, "@rpath/libsundials_sunlinsolband.4.5.1.dylib")
+JLLWrappers.@declare_library_product(libsundials_sunlinsoldense, "@rpath/libsundials_sunlinsoldense.4.5.1.dylib")
+JLLWrappers.@declare_library_product(libsundials_sunlinsolklu, "@rpath/libsundials_sunlinsolklu.4.5.1.dylib")
+JLLWrappers.@declare_library_product(libsundials_sunlinsollapackband, "@rpath/libsundials_sunlinsollapackband.4.5.1.dylib")
+JLLWrappers.@declare_library_product(libsundials_sunlinsollapackdense, "@rpath/libsundials_sunlinsollapackdense.4.5.1.dylib")
+JLLWrappers.@declare_library_product(libsundials_sunlinsolpcg, "@rpath/libsundials_sunlinsolpcg.4.5.1.dylib")
+JLLWrappers.@declare_library_product(libsundials_sunlinsolspbcgs, "@rpath/libsundials_sunlinsolspbcgs.4.5.1.dylib")
+JLLWrappers.@declare_library_product(libsundials_sunlinsolspfgmr, "@rpath/libsundials_sunlinsolspfgmr.4.5.1.dylib")
+JLLWrappers.@declare_library_product(libsundials_sunlinsolspgmr, "@rpath/libsundials_sunlinsolspgmr.4.5.1.dylib")
+JLLWrappers.@declare_library_product(libsundials_sunlinsolsptfqmr, "@rpath/libsundials_sunlinsolsptfqmr.4.5.1.dylib")
+JLLWrappers.@declare_library_product(libsundials_sunmatrixband, "@rpath/libsundials_sunmatrixband.4.dylib")
+JLLWrappers.@declare_library_product(libsundials_sunmatrixdense, "@rpath/libsundials_sunmatrixdense.4.dylib")
+JLLWrappers.@declare_library_product(libsundials_sunmatrixsparse, "@rpath/libsundials_sunmatrixsparse.4.dylib")
+JLLWrappers.@declare_library_product(libsundials_sunnonlinsolfixedpoint, "@rpath/libsundials_sunnonlinsolfixedpoint.3.5.1.dylib")
+JLLWrappers.@declare_library_product(libsundials_sunnonlinsolnewton, "@rpath/libsundials_sunnonlinsolnewton.3.5.1.dylib")
 function __init__()
     JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, libblastrampoline_jll, SuiteSparse_jll)
     JLLWrappers.@init_library_product(
         libsundials_arkode,
-        "lib/libsundials_arkode.4.2.0.dylib",
+        "lib/libsundials_arkode.5.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_cvode,
-        "lib/libsundials_cvode.5.2.0.dylib",
+        "lib/libsundials_cvode.6.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_cvodes,
-        "lib/libsundials_cvodes.5.2.0.dylib",
+        "lib/libsundials_cvodes.6.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_ida,
-        "lib/libsundials_ida.5.2.0.dylib",
+        "lib/libsundials_ida.6.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_idas,
-        "lib/libsundials_idas.4.2.0.dylib",
+        "lib/libsundials_idas.5.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_kinsol,
-        "lib/libsundials_kinsol.5.2.0.dylib",
+        "lib/libsundials_kinsol.6.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_nvecmanyvector,
-        "lib/libsundials_nvecmanyvector.5.2.0.dylib",
+        "lib/libsundials_nvecmanyvector.6.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_nvecserial,
-        "lib/libsundials_nvecserial.5.2.0.dylib",
+        "lib/libsundials_nvecserial.6.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_sunlinsolband,
-        "lib/libsundials_sunlinsolband.3.2.0.dylib",
+        "lib/libsundials_sunlinsolband.4.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_sunlinsoldense,
-        "lib/libsundials_sunlinsoldense.3.2.0.dylib",
+        "lib/libsundials_sunlinsoldense.4.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_sunlinsolklu,
-        "lib/libsundials_sunlinsolklu.3.2.0.dylib",
+        "lib/libsundials_sunlinsolklu.4.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_sunlinsollapackband,
-        "lib/libsundials_sunlinsollapackband.3.2.0.dylib",
+        "lib/libsundials_sunlinsollapackband.4.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_sunlinsollapackdense,
-        "lib/libsundials_sunlinsollapackdense.3.2.0.dylib",
+        "lib/libsundials_sunlinsollapackdense.4.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_sunlinsolpcg,
-        "lib/libsundials_sunlinsolpcg.3.2.0.dylib",
+        "lib/libsundials_sunlinsolpcg.4.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_sunlinsolspbcgs,
-        "lib/libsundials_sunlinsolspbcgs.3.2.0.dylib",
+        "lib/libsundials_sunlinsolspbcgs.4.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_sunlinsolspfgmr,
-        "lib/libsundials_sunlinsolspfgmr.3.2.0.dylib",
+        "lib/libsundials_sunlinsolspfgmr.4.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_sunlinsolspgmr,
-        "lib/libsundials_sunlinsolspgmr.3.2.0.dylib",
+        "lib/libsundials_sunlinsolspgmr.4.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_sunlinsolsptfqmr,
-        "lib/libsundials_sunlinsolsptfqmr.3.2.0.dylib",
+        "lib/libsundials_sunlinsolsptfqmr.4.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_sunmatrixband,
-        "lib/libsundials_sunmatrixband.3.2.0.dylib",
+        "lib/libsundials_sunmatrixband.4.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_sunmatrixdense,
-        "lib/libsundials_sunmatrixdense.3.2.0.dylib",
+        "lib/libsundials_sunmatrixdense.4.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_sunmatrixsparse,
-        "lib/libsundials_sunmatrixsparse.3.2.0.dylib",
+        "lib/libsundials_sunmatrixsparse.4.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_sunnonlinsolfixedpoint,
-        "lib/libsundials_sunnonlinsolfixedpoint.2.2.0.dylib",
+        "lib/libsundials_sunnonlinsolfixedpoint.3.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsundials_sunnonlinsolnewton,
-        "lib/libsundials_sunnonlinsolnewton.2.2.0.dylib",
+        "lib/libsundials_sunnonlinsolnewton.3.5.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
